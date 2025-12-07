d27a533ca9f02218107f9fffbadd66d7ae1c650d
bibakroy <roybibak1@gmail.com>
2025-12-08 03:22:17 +0600

    add schema to sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,9 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import type { SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { courseType } from "./courseType";
+import { lessonType } from "./lessonType";
+import { moduleType } from "./moduleType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [courseType, moduleType, lessonType, categoryType],
+};
